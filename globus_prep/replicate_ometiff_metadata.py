8d16b813df2086e2a945339e1cbf662e9a1619ff
kyu <kyuhan14@gmail.com>
2024-05-02 10:52:23 -0400

    s

--- globus_prep/replicate_ometiff_metadata.py
+++ globus_prep/replicate_ometiff_metadata.py
@@ -1,7 +1,11 @@
 import os
 from shutil import copy
 
-csvfn = r"\\10.99.68.54\Digital pathology image lib\HubMap Skin TMC project\Data_upload\D009\lab_processed\images\z-0001.ome-tiff.channels.csv"
-for i in range(99):
-    n = i+2
-    copy(csvfn,csvfn.replace('z-0001','z-{}'.format(str(n).zfill(4))))
+csvfn = r"\\10.99.68.54\Digital pathology image lib\HubMap Skin TMC project\bulk_slide_registration\imname.ome-tiff.channels.csv"
+dst = r'\\10.99.68.54\Digital pathology image lib\HubMap Skin TMC project\Data_upload\D006B008\lab_processed\images'
+ims = [_ for _ in os.listdir(dst) if _.endswith('ome.tiff')]
+
+for im in ims:
+    imnm, ext = os.path.splitext(im)
+    imnm, ext = os.path.splitext(imnm)
+    copy(csvfn,os.path.join(dst,imnm+'.ome-tiff.channels.csv'))
